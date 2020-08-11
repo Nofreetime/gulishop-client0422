6501d9c01288a1efff84c2d07b31673e255b7e0a
time <291983196@qq.com>
2020-08-11 12:45:16 +0800

    hello

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
+    <!-- 使用 -->
+    <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-
+// 导入，引入
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default {
   name:'App',
+  components:{
+    // 注册
+    Header,
+    Footer
+  }
 }
 </script>
 
